8912c14e87ce3905de987f6644057dcaa78aa221
mfaydin49 <mfaydin49@gmail.com>
2022-03-18 00:47:12 +0100

    added redux for alert

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,10 @@
 import React, { Fragment, useState } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import { setAlert } from "../../actions/alert";
 
-const Register = () => {
+const Register = (props) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -17,7 +19,7 @@ const Register = () => {
   const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
-      console.log("Passwords do not match");
+      props.setAlert("Passwords do not match", "danger");
     } else {
       console.log("Success");
       /* const newUser = { name, email, password };
@@ -98,4 +100,4 @@ const Register = () => {
     </Fragment>
   );
 };
-export default Register;
+export default connect(null, { setAlert })(Register);
